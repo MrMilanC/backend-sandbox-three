ada6cf9b65b7d3b406f18804201d092c02a4450b
vidakovicm94 <mirjana.vidakovic@gmx.at>
2023-10-22 23:59:12 +0200

    testing - user service test and login controller test and login controller modification

--- src/main/java/com/example/backendsandboxthree/controller/LoginController.java
+++ src/main/java/com/example/backendsandboxthree/controller/LoginController.java
@@ -5,6 +5,7 @@ import com.example.backendsandboxthree.dto.LoginResponse;
 import com.example.backendsandboxthree.service.LoginService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,20 +17,14 @@ public class LoginController {
     private final LoginService loginService;
 
     @PostMapping("/login")
-    public LoginResponse login(@RequestBody @Valid LoginRequest loginRequest) {
-
+    public ResponseEntity<LoginResponse> login(@RequestBody @Valid LoginRequest loginRequest) {
         String token = loginService.authenticateUser(loginRequest);
-
-        System.out.println("Token gemacht");
-        System.out.println(loginRequest.getUsername());
-        System.out.println(loginRequest.getPassword());
-        System.out.println(token);
-
-        return LoginResponse.builder()
+        return ResponseEntity.ok(LoginResponse.builder()
                 .accessToken(token)
                 .ok(true)
-                .build();
+                .build());
     }
+
 }
 
 
