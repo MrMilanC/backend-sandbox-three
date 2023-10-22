da8b6169ef9701eea30e8d663a69bd87a5576476
vidakovicm94 <mirjana.vidakovic@gmx.at>
2023-10-22 21:57:16 +0200

    testing controller

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,10 +1,13 @@
 INSERT INTO user (first_name, last_name, password, role, username, email) VALUES ('Max', 'Mustermann', '$2a$12$FEwVGMzao/HRCbgKk1pAdurBLs.4VvxU0JkDM54x.Giw5pZTlh/Nu', 'ROLE_USER', 'user', 'max@email.at');
 INSERT INTO user (first_name, last_name, password, role, username, email) VALUES ('Dolores', 'Bauer', '$2a$12$FEwVGMzao/HRCbgKk1pAdurBLs.4VvxU0JkDM54x.Giw5pZTlh/Nu', 'ROLE_ADMIN', 'admin', 'dolores@email.at');
 
+
 INSERT INTO category (category_name) VALUES ('Saefte');
 INSERT INTO category (category_name) VALUES ('Gemuese');
 INSERT INTO category (category_name) VALUES ('Obst');
 INSERT INTO category (category_name) VALUES ('Tierprodukte');
+
+
 
 INSERT INTO product (description, image_name, price, product_name, category_id, quantity) VALUES ('Orangensaft pur', 'juiceOrange.jpg', 2.35, 'Orangensaft', 1, 7);
 INSERT INTO product (description, image_name, price, product_name, category_id, quantity) VALUES ('frische Paprika', 'paprika.jpg', 4.85, 'Paprika', 2, 10);
@@ -14,3 +17,5 @@ INSERT INTO product (description, image_name, price, product_name, category_id, 
 INSERT INTO product (description, image_name, price, product_name, category_id, quantity) VALUES ('frische Karotten', 'karotte.jpg', 2.35, 'Karotte', 2, 7);
 INSERT INTO product (description, image_name, price, product_name, category_id, quantity) VALUES ('frischer Käse', 'cheese.jpg', 7.95, 'Käse', 4, 7);
 INSERT INTO product (description, image_name, price, product_name, category_id, quantity) VALUES ('Apfelsaft pur', 'juiceApple.jpg', 6.75, 'Apfelsaft', 1, 7);
+
+INSERT INTO user (first_name, last_name, password, role, username, email) VALUES ('Max', 'Mustermann', '$2a$12$FEwVGMzao/HRCbgKk1pAdurBLs.4VvxU0JkDM54x.Giw5pZTlh/Nu', 'ROLE_USER', 'user', 'max@email.at');
